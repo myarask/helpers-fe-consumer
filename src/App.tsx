e16c968691d6f1cab285ef4bddfb8b9cb2da975d
myarask <myaraskavitch@gobonfire.com>
2021-01-28 13:03:22 -0500

    add auth0

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import * as React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
+  const { isLoading, isAuthenticated, error, user, loginWithRedirect, logout } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Oops... {error.message}</div>;
+  }
+  if (!isAuthenticated) {
+    return <button onClick={loginWithRedirect}>Log in</button>;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -14,6 +27,7 @@ const App = () => {
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
+        <button onClick={() => logout({ returnTo: window.location.origin })}>Log out</button>
       </header>
     </div>
   );
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="react-scripts" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: 'development' | 'production' | 'test';
+    PUBLIC_URL: string;
+    REACT_APP_AUTH_DOMAIN: string;
+    REACT_APP_AUTH_CLIENT_ID: string;
+    REACT_APP_AUTH_AUDIENCE: string;
+    REACT_APP_STRIPE_KEY: string;
+  }
+}
